7fc3c20e751fa52fa6e4fca4b6ceb1ea3b551fda
david <xuj13@rpi.edu>
2020-05-11 08:26:11 +0800

    error anlysis

--- errorcompartion.py
+++ errorcompartion.py
@@ -1,9 +1,7 @@
 #import packages
 import csv
 import pandas as pd
-import seaborn as sns
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
@@ -12,8 +10,11 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
 from sklearn import metrics
+import time
 
-wine = pd.read_csv('data.csv', sep=',')
+
+tic = time.perf_counter()
+wine = pd.read_csv('dataComplextestC1.csv', sep=',')
 
 X = wine.drop('tau1', axis=1)
 y = wine['tau1']
@@ -31,7 +32,7 @@ hiddenlayerwanted = 6
 while i < hiddenlayerwanted:
     # hidden layer modify
 
-    #train and test splitting of data
+   # train and test splitting of data
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.25, random_state=42)
 
@@ -41,7 +42,7 @@ while i < hiddenlayerwanted:
     X_test = sc.transform(X_test)
 
     mlpc = MLPRegressor(activation='relu', solver='lbfgs', alpha=0.001,
-                        hidden_layer_sizes=hiddenlayerinit, max_iter=10000)
+                        hidden_layer_sizes=hiddenlayerinit, max_iter=1000000000000000)
     mlpc.fit(X_train, y_train)
     pred_mlpc = mlpc.predict(X_test)
     #print(pred_mlpc)
@@ -68,6 +69,11 @@ with open("errorcompare.csv", 'w') as outfile:
     writer.writerow(["train_R", "train_RSE", "test_R", "test_RSE"])
     rows = zip(train_R, train_MSE, test_R, test_MSE)
     writer.writerows(rows)
+
+toc = time.perf_counter()
+
+print(f"task took {toc - tic:0.4f} seconds")
+
 #plt.scatter(X_temp,pred_mlpc)
 
 # print("weights between input and first hidden layer:")
