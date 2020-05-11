d4916c9425bcd32e854008863afacce0290adfda
luttyest <35268926+luttyest@users.noreply.github.com>
2020-05-11 14:36:36 +0800

    Update formalmodel.py

--- formalmodel.py
+++ formalmodel.py
@@ -17,29 +17,59 @@ import pickle
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import KFold
-from sklearn import model_selection
+
+# k fold cross validation
 
 tic = time.perf_counter()
+
+# different alpha levels
+alphas = [0.001, 0.01, 0.1, 1, 10]
+print('All errors are RMSE')
 
 wine = pd.read_csv('dataComplextestC1.csv', sep=',')
 pd.isnull(wine).sum() > 0
 #wine.replace([np.inf, -np.inf], np.nan, inplace=True)
 
 wine.replace([np.inf, -np.inf], np.nan).dropna(how="all")
-X = wine.drop('tau1', axis=1)
-y = wine['tau1']
+data = wine.drop('tau1', axis=1)
+target = wine['tau1']
 
+K = 10
 #cross validation
-from sklearn.model_selection import ShuffleSplit
-from sklearn.linear_model import LogisticRegression
+kf = KFold(n_splits=K, shuffle=True, random_state=42)
 
+for alpha in alphas:
+    train_errors = []
+    validation_errors = []
+    for train_index, val_index in kf.split(data, target):
+        
+        # split data
+        X_train, X_val = data[train_index], data[val_index]
+        y_train, y_val = target[train_index], target[val_index]
 
-kfold2 = model_selection.ShuffleSplit(n_splits=10, test_size=0.20, random_state=100)
-model_shufflecv = MLPRegressor(activation='relu', solver='lbfgs',alpha=0.001, hidden_layer_sizes=(30,30,30,30,30), max_iter=10000000000000000000000)
-sc = StandardScaler()
-X = sc.fit_transform(X)
-results_4 = model_selection.cross_val_score(model_shufflecv, X, y, cv=kfold2)
-print("Accuracy: %.2f%% (%.2f%%)" % (results_4.mean()*100.0, results_4.std()*100.0))
+        # instantiate model
+        model_shufflecv = MLPRegressor(activation='relu', solver='lbfgs',alpha=alpha, hidden_layer_sizes=(30,30,30,30,30), max_iter=10000000000000000000000)
+        
+        #calculate errors
+        train_error, val_error = calc_metrics(X_train, y_train, X_val, y_val, lasso)
+        
+        # append to appropriate list
+        train_errors.append(train_error)
+        validation_errors.append(val_error)
+    
+    # generate report
+    print('alpha: {:6} | mean(train_error): {:7} | mean(val_error): {}'.
+          format(alpha,
+                 round(np.mean(train_errors),4),
+                 round(np.mean(validation_errors),4)))
+
+    
+# kfold2 = model_selection.ShuffleSplit(n_splits=10, test_size=0.20, random_state=100)
+# model_shufflecv = MLPRegressor(activation='relu', solver='lbfgs',alpha=0.001, hidden_layer_sizes=(30,30,30,30,30), max_iter=10000000000000000000000)
+# sc = StandardScaler()
+# X = sc.fit_transform(X)
+# results_4 = model_selection.cross_val_score(model_shufflecv, X, y, cv=kfold2)
+# print("Accuracy: %.2f%% (%.2f%%)" % (results_4.mean()*100.0, results_4.std()*100.0))
 
 
 
